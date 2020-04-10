8cce66d49165227c87c8f3448007721e3bd6d771
JonyEldan <eldan.yonatan@gmail.com>
2020-04-10 13:27:48 +0300

    Finished updateEdgeMatrix function

--- src/edu/cg/SeamsCarver.java
+++ src/edu/cg/SeamsCarver.java
@@ -18,12 +18,9 @@ public class SeamsCarver extends ImageProcessor {
 	boolean[][] imageMask;
 	ArrayList<Pixel>[] edges;
 	int[][] greyScale;
-	boolean[][] seamsMatrix;
+	boolean[][] seams;
 
-	// the last seam that was found
 	int[] lastSeam;
-
-	// TODO: Add some additional fields
 
 	public SeamsCarver(Logger logger, BufferedImage workingImage, int outWidth, RGBWeights rgbWeights,
 			boolean[][] imageMask) {
@@ -54,7 +51,7 @@ public class SeamsCarver extends ImageProcessor {
 			initEdgesMatrix();
 
 			// init some data structure to store all the k seams.
-			initSeamsVars();
+			initSeamsMatrix();
 
 			// find the 𝑘 most minimal seams
 			findKSeams();
@@ -89,13 +86,40 @@ public class SeamsCarver extends ImageProcessor {
 			}
 		}
 	}
+	private void updateEdgeMatrix() {
+		//remove seam from edges matrix
+		for (int i = 0; i < lastSeam.length; i++) {
+			this.edges[i].remove(lastSeam[i]);
+		}
+		//calc new magnitude for the new edges matrix
+		for (int y = 0; y < edges.length; y++) {
+			for (int x = 0; x < edges[y].size(); x++) {
+				this.edges[y].get(x).magnitude = updateMagnitude(this.edges[y].get(x));
+			}
+		}
 
-	private void initSeamsVars() {
-		this.seamsMatrix = new boolean[inHeight][inWidth];
-		this.lastSeam = new int[inHeight];
+	}
+
+	private void initSeamsMatrix() {
+		this.seams = new boolean[inHeight][inWidth];
 	}
 
 	private int calcMagnitude(Pixel pixel) {
+		int magnitude = (int)Math.sqrt(Math.pow(this.greyScale[pixel.x+1][pixel.y] - this.greyScale[pixel.x][pixel.y], 2) + (Math.pow(this.greyScale[pixel.x][pixel.y+1] - this.greyScale[pixel.x][pixel.y], 2)));
+		if(pixel.y == inHeight -1 && pixel.x == inWidth -1 ) {
+			magnitude = (int)Math.sqrt(Math.pow(this.greyScale[pixel.x-1][pixel.y] - this.greyScale[pixel.x][pixel.y], 2) + (Math.pow(this.greyScale[pixel.x][pixel.y-1] - this.greyScale[pixel.x][pixel.y], 2)));
+		}
+		else {
+			if (pixel.x == inWidth - 1) {
+				magnitude = (int) Math.sqrt(Math.pow(this.greyScale[pixel.x - 1][pixel.y] - this.greyScale[pixel.x][pixel.y], 2) + (Math.pow(this.greyScale[pixel.x][pixel.y + 1] - this.greyScale[pixel.x][pixel.y], 2)));
+			}
+			if (pixel.y == inHeight - 1) {
+				magnitude = (int) Math.sqrt((this.greyScale[pixel.x + 1][pixel.y] - this.greyScale[pixel.x][pixel.y]) + (Math.pow(this.greyScale[pixel.x][pixel.y - 1] - this.greyScale[pixel.x][pixel.y], 2)));
+			}
+		}
+		return magnitude;
+	}
+	private int updateMagnitude(Pixel pixel) {
 		int magnitude, xMinus1, yMinus1, xPlus1, yPlus1;
 		if(pixel.y == inHeight -1 && pixel.x == inWidth -1 ) {
 			xMinus1 = edges[pixel.y].get(pixel.x -1).x;
@@ -122,28 +146,10 @@ public class SeamsCarver extends ImageProcessor {
 		return magnitude;
 	}
 
-
-
-
 	private void findKSeams() {
 		for (int i = 0; i < numOfSeams ; i++) {
 			findMinimalSeam(i);
-			updateEdgeMatrix();
 		}
-	}
-
-	private void updateEdgeMatrix() {
-		//remove seam from edges matrix
-		for (int i = 0; i < lastSeam.length; i++) {
-			this.edges[i].remove(lastSeam[i]);
-		}
-		//calc new magnitude for the new edges matrix
-		for (int y = 0; y < edges.length; y++) {
-			for (int x = 0; x < edges[y].size(); x++) {
-				this.edges[y].get(x).magnitude = calcMagnitude(this.edges[y].get(x));
-			}
-		}
-
 	}
 
 	/*find a minimal seam using the edges matrix
@@ -157,55 +163,26 @@ public class SeamsCarver extends ImageProcessor {
 		//fill the matrix
         for (int y = 0; y < costMat.length ; y++) {
             for (int x = 0; x < costMat[0].length; x++) {
-
-                costMat[y][x] = edges[y].get(x).getPixelEnergy();
-
-                // fill the first row without considering cl, cv of cr.
-                if (y == 0) continue;
-
-				int cl = 0;
-				int cv = 0;
-				int cr = 0;
+                // fill the first row
+                if (y == 0) {
+                    costMat[y][x] = edges[y].get(x).getPixelEnergy();
+                }
 
                 //left most pixel in the row
                 if (x == 0){
-                    // find how to calc cv here
-                    //cv = (int)   Math.sqrt(Math.pow(this.greyScale[y][x-1] - greyScale[y][x+1], 2));
 
-                    cr = (int) Math.sqrt(Math.pow(this.greyScale[y][x+1] - greyScale[y-1][x], 2));
-
-                    costMat[y][x] += Math.min( costMat[y-1][x], costMat[y-1][x+1] + cr);
-				}
+                }
                 // right most pixel in the row
                 else if (x == costMat[0].length - 1){
-                    // find how to calc cv here
-                    //cv = (int)   Math.sqrt(Math.pow(this.greyScale[y][x-1] - greyScale[y][x+1], 2));
 
-                    cl = (int)  Math.sqrt(Math.pow(this.greyScale[y-1][x] - greyScale[y][x-1], 2));
-
-                    costMat[y][x] += Math.min( costMat[y-1][x], costMat[y-1][x-1] + cl);
                 }
                 else {
-                    cl = (int) ( Math.sqrt(Math.pow(this.greyScale[y][x-1] - greyScale[y][x+1], 2)) +
-                                 Math.sqrt(Math.pow(this.greyScale[y-1][x] - greyScale[y][x-1], 2)) );
 
-                    cv = (int)   Math.sqrt(Math.pow(this.greyScale[y][x-1] - greyScale[y][x+1], 2));
-
-                    cr = (int) ( Math.sqrt(Math.pow(this.greyScale[y][x-1] - greyScale[y][x+1], 2)) +
-                                 Math.sqrt(Math.pow(this.greyScale[y][x+1] - greyScale[y-1][x], 2)) );
-
-                    costMat[y][x] += Math.min(costMat[y-1][x-1] + cl,
-                                     Math.min(costMat[y-1][x] + cv,
-                                              costMat[y-1][x+1] + cr));
                 }
             }
         }
 
-        //trace back in the cost matrix to find the minimal seam:
 
-
-
-        //insert the seam to the DS
 
 	}
 
